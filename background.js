8e34905a9b773eb7a448e44642690f2053c53f87
impermaxvict <35343365+impermaxvict@users.noreply.github.com>
2020-01-05 11:43:06 +0100

    Implemented request blocker

--- background.js
+++ background.js
@@ -6,7 +6,46 @@ browser.runtime.onInstalled.addListener(() => {
 	});
 });
 
+let domainWhitelist = [];
+let domainBlacklist = [];
+
+browser.storage.local.get('domainWhitelist').then(results => {
+	if (results.domainWhitelist) {
+		domainWhitelist = results.domainWhitelist;
+	}
+});
+
+browser.storage.local.get('domainBlacklist').then(results => {
+	if (results.domainBlacklist) {
+		domainBlacklist = results.domainBlacklist;
+	}
+});
+
+browser.storage.onChanged.addListener(function (changes, area) {
+	if (area === 'local') {
+		if (changes.domainWhitelist && changes.domainWhitelist.newValue) {
+			domainWhitelist = changes.domainWhitelist.newValue;
+		}
+
+		if (changes.domainBlacklist && changes.domainBlacklist.newValue) {
+			domainBlacklist = changes.domainBlacklist.newValue;
+		}
+	}
+});
+
 function isDomainBlocked(domain) {
+	if (domainWhitelist.includes(domain)) {
+		// exact in whitelist
+		return false;
+	} else if (domainBlacklist.includes(domain)) {
+		// exact in blacklist
+		return true;
+	} else if (domain.includes('.')) {
+		// check next level
+		const nextLevel = domain.substring(domain.indexOf('.'));
+		return isDomainBlocked(nextLevel);
+	}
+	// unknown domain
 	return false;
 }
 
@@ -14,6 +53,7 @@ browser.webRequest.onBeforeRequest.addListener(
 	function (requestDetails) {
 		const requestDomain = new URL(requestDetails.url).hostname;
 
+		// TODO: make this more efficient
 		if (requestDomain) {
 			addDomainToSet(requestDomain, 'seenDomains');
 		}
