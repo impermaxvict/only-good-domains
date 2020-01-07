973a95fafd8686555d4e1a34e6038897a2e504ca
impermaxvict <35343365+impermaxvict@users.noreply.github.com>
2020-01-07 13:17:09 +0100

    Changed background script to use sets instead of arrays

--- background.js
+++ background.js
@@ -6,38 +6,38 @@ browser.runtime.onInstalled.addListener(() => {
 	});
 });
 
-let domainWhitelist = [];
-let domainBlacklist = [];
+let domainWhitelist = new Set();
+let domainBlacklist = new Set();
 
 browser.storage.local.get('domainWhitelist').then(results => {
 	if (results.domainWhitelist) {
-		domainWhitelist = results.domainWhitelist;
+		domainWhitelist = new Set(results.domainWhitelist);
 	}
 });
 
 browser.storage.local.get('domainBlacklist').then(results => {
 	if (results.domainBlacklist) {
-		domainBlacklist = results.domainBlacklist;
+		domainBlacklist = new Set(results.domainBlacklist);
 	}
 });
 
 browser.storage.onChanged.addListener(function (changes, area) {
 	if (area === 'local') {
 		if (changes.domainWhitelist && changes.domainWhitelist.newValue) {
-			domainWhitelist = changes.domainWhitelist.newValue;
+			domainWhitelist = new Set(changes.domainWhitelist.newValue);
 		}
 
 		if (changes.domainBlacklist && changes.domainBlacklist.newValue) {
-			domainBlacklist = changes.domainBlacklist.newValue;
+			domainBlacklist = new Set(changes.domainBlacklist.newValue);
 		}
 	}
 });
 
 function isDomainBlocked(domain) {
-	if (domainWhitelist.includes(domain)) {
+	if (domainWhitelist.has(domain)) {
 		// exact in whitelist
 		return false;
-	} else if (domainBlacklist.includes(domain)) {
+	} else if (domainBlacklist.has(domain)) {
 		// exact in blacklist
 		return true;
 	} else if (domain.includes('.')) {
